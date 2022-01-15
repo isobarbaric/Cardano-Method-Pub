0ac655b788343b5293e5e6cca58713d714ac4c57
isobarbaric <shahkrish2016@gmail.com>
2022-01-15 18:17:47 -0500

    all arithmetic operations are now operational in the complex class

--- complex.py
+++ complex.py
@@ -15,26 +15,17 @@ class complex:
         return complex(real_part, imaginary_part)
 
     def __mul__(self, other):
-        real_part = self.real*other.real + self.imaginary*other.imaginary
+        real_part = self.real*other.real - self.imaginary*other.imaginary
         imaginary_part = self.real*other.imaginary + self.imaginary*other.real
         return complex(real_part, imaginary_part)
         
     def __truediv__(self, other):
-        div_by = (other.real ** 2) - (other.imaginary ** 2)
-        print(div_by)
-        if self.imaginary > 0:
-            real_part = (complex(self.real, 0)*complex(other.real, -1*other.imaginary))/div_by
-            imaginary_part = (complex(0, self.imaginary)*complex(other.real, -1*other.imaginary))/div_by
-        else:
-            real_part = (complex(self.real, 0)*complex(other.real, -1*other.imaginary))/div_by
-            imaginary_part = (complex(0, self.imaginary)*complex(other.real, -1*other.imaginary))/div_by
+        numerator = self*complex(other.real, -1*other.imaginary)
+        denominator = (other.real ** 2) + (other.imaginary ** 2)
+        return complex(numerator.real/denominator, numerator.imaginary/denominator)
             
     def __str__(self):
         sign = '+'
         if (self.imaginary < 0):
             sign = '-'
         return ("{}" + sign + "{}i").format(self.real, abs(self.imaginary))
-
-p1 = complex(1, -2)
-p2 = complex(3, 1)
-print(p1/p2)
