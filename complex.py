a56353b7f2eaaac71cca6bc5b6edf1605a9e551b
isobarbaric <shahkrish2016@gmail.com>
2022-01-15 18:01:43 -0500

    addendum

--- complex.py
+++ complex.py
@@ -15,21 +15,26 @@ class complex:
         return complex(real_part, imaginary_part)
 
     def __mul__(self, other):
-        real_part = self.real*other.real - self.imaginary*other.imaginary
+        real_part = self.real*other.real + self.imaginary*other.imaginary
         imaginary_part = self.real*other.imaginary + self.imaginary*other.real
         return complex(real_part, imaginary_part)
         
     def __truediv__(self, other):
-        div_by = other.real*other.real - other.imaginary*other.imaginary
-        real_part = (self.real*complex(other.real, -1*other.imaginary)) 
-        imaginary_part = (self.imaginary*complex(other.real, -1*other.imaginary))/div_by
-
+        div_by = (other.real ** 2) - (other.imaginary ** 2)
+        print(div_by)
+        if self.imaginary > 0:
+            real_part = (complex(self.real, 0)*complex(other.real, -1*other.imaginary))/div_by
+            imaginary_part = (complex(0, self.imaginary)*complex(other.real, -1*other.imaginary))/div_by
+        else:
+            real_part = (complex(self.real, 0)*complex(other.real, -1*other.imaginary))/div_by
+            imaginary_part = (complex(0, self.imaginary)*complex(other.real, -1*other.imaginary))/div_by
+            
     def __str__(self):
         sign = '+'
         if (self.imaginary < 0):
             sign = '-'
         return ("{}" + sign + "{}i").format(self.real, abs(self.imaginary))
 
-p1 = complex(3, -5)
-p2 = complex(5, 32)
-print(p1*p2)
+p1 = complex(1, -2)
+p2 = complex(3, 1)
+print(p1/p2)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from depress_cubic import modify
 
-coefficients = [int(i) for i in input("Enter the cubic polynomial's coefficients in order of increasing degree separated by spaces. Any vanishing terms should be included with the coefficient zero.").split()]
 # list indices 'i' map directly to a_i notation 
+coefficients = [int(i) for i in input("Enter the cubic polynomial's coefficients in order of increasing degree separated by spaces. Any vanishing terms should be included with the coefficient zero.").split()]
 
 if (coefficients[2] != 0):
     coefficients = modify(coefficients)
