b7e56f9e267466dd45d9bc0f945d98f19ffc4052
isobarbaric <shahkrish2016@gmail.com>
2022-01-20 22:30:08 -0500

    modified original proj to interface COVID-19 data as well

---
+++ covid.py
@@ -0,0 +1,38 @@
+import numpy as np
+import requests
+from datetime import date, timedelta
+import matplotlib.pyplot as plt
+
+def covid_graph():
+    reference_indices = []
+    daily_totals = []
+
+    start_date = date(2021, 11, 29)
+    end_date = date.today()
+    delta = timedelta(days=1)
+    html_file = requests.get('https://health-infobase.canada.ca/src/data/covidLive/covid19.json').json()
+    
+    while start_date <= end_date:
+        for i in html_file:
+            if i['prname'] == 'Canada' and i['date'] == start_date.strftime("%d-%m-%Y"):
+                daily_totals.append(int(i['numtoday']))
+                break
+        start_date += delta
+    
+    for i in range(1, len(daily_totals)+1):
+        reference_indices.append(i)
+
+    for i in range(len(daily_totals)):
+        print(reference_indices[i], daily_totals[i])
+
+    # polynomial fit with degree = 3
+    model = np.poly1d(np.polyfit(reference_indices, daily_totals, 3))
+    print(model)
+    cf = [float(i) for i in model.c]
+
+    # polyline = np.linspace(1, 60, 50)
+    # plt.scatter(reference_indices, daily_totals)
+    # plt.plot(polyline, model(polyline))
+    # plt.show()
+
+    return cf
--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 from depress_cubic import *
 from complex import *
 from cardano import *
+from covid import *
 
-# list indices 'i' map directly to a_i notation 
-coeff = [int(i) for i in input("Enter the cubic polynomial's coefficients in order of increasing degree separated by spaces. Any vanishing terms should be included with the coefficient zero.\n").split()]
-
-# mention real coefficients only 
+coeff = covid_graph()
+coeff.reverse()
 
 coeff, H, G, shift = depressed_cubic(coeff)
+
+print(coeff)
 
 answers = cardano_method(H, G, shift)
 
