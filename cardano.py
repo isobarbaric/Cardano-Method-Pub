28077948552e2c8bec940dc74e2e040c9714f2a8
isobarbaric <shahkrish2016@gmail.com>
2022-01-22 00:14:30 -0500

    added rounding functionality to omega and omega sq.

--- cardano.py
+++ cardano.py
@@ -9,7 +9,7 @@ def cardano_method(H, G, shift):
     u, v = quadratic(complex(1), complex(G), complex(-(H ** 3))) 
     first = round(u.root(3))
     second = round(complex(-H)/u.root(3))
-    omega, omega_sq = quadratic(complex(1), complex(1), complex(1))
+    omega, omega_sq = [round(i) for i in quadratic(complex(1), complex(1), complex(1))]
     withoutShift = [round(first+second), round(omega*first+omega_sq*second), round(omega_sq*first+omega*second)]
     answers = [round(i-complex(shift)) for i in withoutShift]
     return answers 
