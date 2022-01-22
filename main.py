2f665a61bb776e0c9fc8c918b088d89fe0be7c71
isobarbaric <shahkrish2016@gmail.com>
2022-01-22 00:49:00 -0500

    wrote a basic generator that writes to a json file that can easily be read from

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 
-# testing 
+
+import random 
+import json
+
+lower_limit = 1
+upper_limit = 750
+
+num_tests = 10000
+
+tests = {}
+
+for i in range(num_tests): 
+    real_part = random.randint(lower_limit, upper_limit)
+    imaginary_part = random.randint(lower_limit, upper_limit)
+    a = complex(real_part, imaginary_part)
+    tests[str(a)] = str(a ** 3)
+
+with open('tests.txt', 'w') as db:
+    data = json.dumps(tests, indent = 4)
+    db.write(data)
